9288dcb8cd98c1e024e8eb3c68170d7cbdca7fdd
newdana01 <newdana01@gmail.com>
2023-06-21 14:35:35 +0900

    Jdbc 연동 추가

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,21 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +24,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository(); // 다른 구현체로 변경해야한다면 이 리턴 값만 변경해주면 됨
+//        return new MemoryMemberRepository(); // 다른 구현체로 변경해야한다면 이 리턴 값만 변경해주면 됨
+        return new JdbcMemberRepository(dataSource);
     }
 }
