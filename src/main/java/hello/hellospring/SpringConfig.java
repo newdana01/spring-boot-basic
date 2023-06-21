b6a0f071b00839f047abd8db928acc31d3b723fc
newdana01 <newdana01@gmail.com>
2023-06-21 17:36:27 +0900

    스프링 데이터 JPA 적용

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -11,21 +11,29 @@ import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
-    private final EntityManager em;
 
-    @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    private final MemberRepository memberRepository;
+
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+
+//    private final EntityManager em;
+//
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+//        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository(); // 다른 구현체로 변경해야한다면 이 리턴 값만 변경해주면 됨
-        return new JpaMemberRepository(em);
-    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+////        return new MemoryMemberRepository(); // 다른 구현체로 변경해야한다면 이 리턴 값만 변경해주면 됨
+//        return new JpaMemberRepository(em);
+//    }
 }
